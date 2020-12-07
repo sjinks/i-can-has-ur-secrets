547db8575c4714c1c1bc43ce4542c10c10fb8b7c
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-12-07 07:09:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// This file is intentionally empty
+// There is no need to require()/import this package,
+// all the work is done in the `install` scripts
+
+console.error("i-can-haz-ur-secrets must not be require()'d");
+process.exit(1);
